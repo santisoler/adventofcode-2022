91b6c61494e3c46cb7a5ec843db21af58213c49f
Santiago Soler <santisoler@fastmail.com>
2022-12-01 18:27:12 -0800

    Solve part 2 of day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -12,6 +12,13 @@ mod tests {
         let result = solve_part1(&fname);
         assert_eq!(result, 24000);
     }
+
+    #[test]
+    fn test_part2() {
+        let fname = String::from("data/test_input");
+        let result = solve_part2(&fname);
+        assert_eq!(result, 45000);
+    }
 }
 
 fn read_file(fname: &String) -> String {
@@ -54,10 +61,54 @@ fn solve_part1(fname: &String) -> u32 {
     highest_calories
 }
 
+fn solve_part2(fname: &String) -> u32 {
+    // Read data file
+    let data = read_file(&fname);
+    // Find out how many calories are being carried by the Elf that carries
+    // the most calories
+    let mut first: u32 = 0;
+    let mut second: u32 = 0;
+    let mut third: u32 = 0;
+    let mut calories: u32 = 0;
+    for line in data.lines() {
+        if line.trim().is_empty() {
+            if calories > first {
+                third = second;
+                second = first;
+                first = calories;
+            } else if calories > second {
+                third = second;
+                second = calories;
+            } else if calories > third {
+                third = calories;
+            }
+            calories = 0;
+        } else {
+            calories += line.parse::<u32>().unwrap();
+        }
+    }
+    // Check again for the last set of calories in the file
+    if calories > first {
+        third = second;
+        second = first;
+        first = calories;
+    } else if calories > second {
+        third = second;
+        second = calories;
+    } else if calories > third {
+        third = calories;
+    }
+    first + second + third
+}
+
 fn main() {
     let fname = String::from("data/input");
 
     // part 1
     let result = solve_part1(&fname);
     println!("Solution to part 1: {}", result);
+
+    // part 2
+    let result = solve_part2(&fname);
+    println!("Solution to part 2: {}", result);
 }
